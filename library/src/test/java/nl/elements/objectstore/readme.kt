f1daf789fad6588b8b8b29c11ab43ae0bbfe4534
DannyLamarti <lamarti.danny@gmail.com>
2019-06-07 15:15:34 +0200

    Update readme

---
+++ library/src/test/java/nl/elements/objectstore/readme.kt
@@ -0,0 +1,69 @@
+package nl.elements.objectstore
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.Bitmap
+import com.facebook.android.crypto.keychain.AndroidConceal
+import com.facebook.android.crypto.keychain.SharedPrefsBackedKeyChain
+import com.facebook.crypto.CryptoConfig
+import nl.elements.objectstore.stores.DirectoryStore
+import nl.elements.objectstore.stores.PreferencesStore
+import nl.elements.objectstore.transformers.ConcealTransformer
+import java.io.File
+import java.util.prefs.Preferences
+
+fun example(store: ObjectStore) {
+    if ("id" !in store)
+        store["id"] = 123L
+
+    val id: Long = store["id"]
+
+    store.remove("id")
+}
+
+fun observe(store: ObjectStore) {
+    store
+        .toObservable()
+        .filter { event -> event.key == "id" }
+        .map { event ->
+            when (event) {
+                is ObjectStore.Event.Updated -> store.get(event.key)
+                is ObjectStore.Event.Removed -> -1L
+            }
+        }
+        .subscribe(::println)
+}
+
+fun conceal(context: Context) {
+    val keyChain = SharedPrefsBackedKeyChain(context, CryptoConfig.KEY_256)
+    val crypto = AndroidConceal.get().createDefaultCrypto(keyChain)
+    val prefs = context.getSharedPreferences("example", Context.MODE_PRIVATE)
+
+    val store = PreferencesStore(
+        preferences = prefs,
+        transformer = ConcealTransformer(crypto)
+    )
+}
+
+fun aggregate(directory: File, preferences: SharedPreferences) {
+    // define the stores
+    val pictures: ObjectStore = DirectoryStore(directory)
+    val config: ObjectStore = PreferencesStore(preferences)
+
+    // reduce them into one store
+    val stores = listOf(pictures, config)
+    val store: ReadableObjectStore = stores.reduce()
+}
+
+fun aggregateWithNamespace(directory: File, preferences: SharedPreferences) {
+    // define the stores
+    val pictures: ObjectStore = DirectoryStore(directory)
+    val config: ObjectStore = PreferencesStore(preferences)
+
+    // reduce them into one store
+    val stores = mapOf("pictures" to pictures, "config" to config)
+    val store: ReadableObjectStore = stores.reduceWithNamespace()
+
+    val picture: Bitmap = store["picture:selfie"]
+    val token: String = store["config:debug"]
+}
